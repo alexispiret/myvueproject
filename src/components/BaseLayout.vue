5bacb9304cd3f0550a65cce5f3b2c410725fbf7f
alexispiret <alexis.piret@efrei.net>
2023-10-20 14:24:24 +0200

    ex 4;5

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <!-- Contenu de l'en-tête -->
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <!-- Contenu du pied de page -->
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "BaseLayout"
+    };
+  }
+}
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,69 @@
+<template>
+  <body>
+    <div class="navbar">
+      <a href="#">Accueil</a>
+      <a href="#">À propos</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
+    </div>
+
+    <footer>
+      <p>&copy; 2023 MyvueProject. Tous droits réservés. | <a href="/politique-de-confidentialite">Politique de confidentialité</a> | <a href="/conditions-dutilisation">Conditions d'utilisation</a></p>
+    </footer>
+  </body>
+</template>
+
+<style>
+body {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+      
+    }
+
+
+.navbar {
+      flex: 1;
+      background-color: #333; 
+      overflow: hidden; /* Pour gérer les éléments flottants à l'intérieur */
+    }
+
+    /* Style pour les liens de la barre de navigation */
+    .navbar a {
+      float: left; 
+      display: block; 
+      color: white; 
+      text-align: center; 
+      padding: 14px 16px; /* Espacement autour du texte */
+      text-decoration: none; 
+    }
+
+    /* Style pour les liens au survol */
+    .navbar a:hover {
+      background-color: #ddd; 
+      color: black; 
+    }
+
+/* Style CSS pour le footer */
+    footer {
+      margin-top: auto;
+      background-color: #333; 
+      height: 50px;
+      color: white; 
+      padding: 10px; /* Espacement autour du texte */
+      text-align: center; 
+    }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "HomePage"
+    };
+  }
+}
+</script>
