6153107cc8bfaedde47f15b6b1c6c7d69a8dfffe
alexispiret <alexis.piret@efrei.net>
2023-10-24 12:26:03 +0200

    ex6save

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from '/src/lib/store.js'
+import router from '/src/router/index.js'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Import components (ConversationsIndexPage) here
+
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
